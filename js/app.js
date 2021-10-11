c1bb92f201611eddaaf86399d8fb888601f8483d
ManliestBen <benjamintmanley@gmail.com>
2021-10-11 14:23:32 -0500

    add defer to HTML script and add init function

--- js/app.js
+++ js/app.js
@@ -7,7 +7,12 @@ let secretNum, guessList, isWinner
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const form = document.querySelector("form")
+const guessInput = document.querySelector("#guess-input")
+const guessesEl = document.querySelector("#prev-guesses")
+const messageEl = document.querySelector("#message")
+const resetBtn = document.querySelector("#reset-button")
+const prevGuessMsg = document.querySelector("#prev-guesses-msg")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +20,25 @@ let secretNum, guessList, isWinner
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+function init(){
+  // Remove all guesses from the guess list
+  guessesEl.innerText = ''
+  // Reset the starting message
+  messageEl.innerText = 'Please enter a number from 1 to 100!' 
+  // Hide the reset button
+  resetBtn.setAttribute('hidden', true)
+  // Get rid of 'Previous Guesses' text
+  prevGuessMsg.innerText = ''
+  // Reset guessList
+  guessList = []
+  // Reset isWinner
+  isWinner = false
+  // Generate new secretNum
+  secretNum = Math.floor(Math.random() * 100 + 1)
+  console.log(secretNum)
+}
+
 
